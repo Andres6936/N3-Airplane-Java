cf1bb6b8022707838c6396567687c2f37a484031
Joan Andrés <andres6936@live.com>
2017-12-03 23:44:06 -0500

    Modify Passenger.java

--- src/edu/jabs/airplane/domain/Passenger.java
+++ src/edu/jabs/airplane/domain/Passenger.java
@@ -1,5 +1,71 @@
 package edu.jabs.airplane.domain;
 
+/**
+ * Passenger airplane
+ */
 public class Passenger
 {
+
+    //-----------------------------------------------------------------
+    // Fields
+    //-----------------------------------------------------------------
+    /**
+     * Passenger id
+     */
+    private int id;
+    /**
+     * Passenger name
+     */
+    private String name;
+
+    //-----------------------------------------------------------------
+    // Constructors
+    //-----------------------------------------------------------------
+
+    /**
+     * It builds a passenger with his document and name<br>
+     * <b>post: </b>The passenger has his basic data document and name assigned.
+     * @param aId - Passenger id - aId >0
+     * @param aName - Passenger Name - aName != ""
+     */
+    public Passenger( int aId, String aName )
+    {
+        id = aId;
+        name = aName;
+    }
+
+    //-----------------------------------------------------------------
+    // Methods
+    //-----------------------------------------------------------------
+
+    /**
+     * It returns the passenger document
+     * @return passenger document
+     */
+    public int getId( )
+    {
+        return id;
+    }
+
+    /**
+     * It returns the passenger name
+     * @return Passenger name
+     */
+    public String getName( )
+    {
+        return name;
+    }
+
+    /**
+     * It points out if the passenger is like another
+     * @param otherPassenger - Passenger to compare - otherPassenger != null
+     * @return true if it is the same passenger, false otherwise
+     */
+    public boolean equalsTo( Passenger otherPassenger )
+    {
+        if( id == otherPassenger.getId( ) )
+            return true;
+        else
+            return false;
+    }
 }
